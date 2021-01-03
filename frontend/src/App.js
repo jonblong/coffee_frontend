0e5cbfb3f77fb1cdcfe1be12cf47b9841de3f4b0
Jon Long <jonbrantleylong@gmail.com>
2021-01-03 01:08:39 -0500

    Added Size Picker

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,17 @@
 import React, { useState, useEffect } from 'react';
-import './App.css'
+import './App.css';
+import SizePicker from './Components/SizePicker/SizePicker';
 
 function App() {
 
   const DATASIZES = {
     BIG: 'big',
-    LAGRE: 'large',
+    LARGE: 'large',
     HUGE: 'huge'
   }
 
   const [drinks, setDrinks] = useState([]);
-  const [dataSize, setDataSize] = useState(DATASIZES.BIG)
+  const [dataSize, setDataSize] = useState(DATASIZES.BIG);
 
   useEffect(() => {
     fetch('https://jon-coffee-api.herokuapp.com/get_drinks')
@@ -22,9 +23,19 @@ function App() {
 
   return (
     <div className="App">
-      <h3 className='subhead'>Jon has consumed</h3>
-      <h2 className='head'>{drinks.length} coffees</h2>
-      <h3 className='subhead'>in 2021.</h3>
+      <SizePicker
+        dataSize={dataSize}
+        setDataSize={setDataSize}
+        sizes={DATASIZES}
+      />
+
+      {dataSize === DATASIZES.BIG &&
+        <div className='big-container'>
+          <h3 className='subhead'>Jon has had</h3>
+          <h2 className='head'>{drinks.length} coffees</h2>
+          <h3 className='subhead'>in 2021.</h3>
+        </div>
+      }
     </div>
   );
 }
